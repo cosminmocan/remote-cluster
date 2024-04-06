9e91698dd02a2fd30684165c9fd52d178eb7cc96
Mocan, Cosmin-Raul <cmocan@deloittece.com>
2024-04-07 02:47:46 +0300

    Second commit

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -49,3 +49,17 @@ resource "azurerm_network_security_rule" "allowSSH" {
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
+
+resource "azurerm_network_security_rule" "allowKube" {
+  name                        = "AllowKubeConfAccess"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "6443"
+  source_address_prefix       = "${chomp(data.http.myip.response_body)}/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
